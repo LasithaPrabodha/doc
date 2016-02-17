220ec6eee0c6eddb1d018f95846fc75f384e33b7
Arun Thomas <arunthms01@gmail.com>
2016-02-18 01:50:04 +0530

    profile added

---
+++ profile/profile.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!--=============================================== 
+        Template Design By WpFreeware Team.
+        Author URI : http://www.wpfreeware.com/
+        ====================================================-->
+
+        <!-- Basic Page Needs
+        ================================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>WpF Medinova : Service</title>
+
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/icon" href="images/favicon.ico"/>
+
+        <!-- CSS
+        ================================================== -->       
+        <!-- Bootstrap css file-->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Font awesome css file-->
+        <link href="css/font-awesome.min.css" rel="stylesheet">       
+        <!-- Default Theme css file -->
+        <link id="switcher" href="css/themes/blue-theme.css" rel="stylesheet">    
+        <!-- Slick slider css file -->
+        <link href="css/slick.css" rel="stylesheet"> 
+
+        <!-- Main structure css file -->
+        <link href="style.css" rel="stylesheet">
+
+        <!-- Google fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>  
+        <link href='http://fonts.googleapis.com/css?family=Habibi' rel='stylesheet' type='text/css'>   
+        <link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative:900' rel='stylesheet' type='text/css'>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]--> 
+    </head>
+    <body> 
+        <!-- BEGAIN PRELOADER -->
+        <div id="preloader">
+            <div id="status">&nbsp;</div>
+        </div>
+        <!-- END PRELOADER -->
+
+        <!-- SCROLL TOP BUTTON -->
+        <a class="scrollToTop" href="#"><i class="fa fa-heartbeat"></i></a>
+        <!-- END SCROLL TOP BUTTON -->
+
+        <!--=========== BEGIN HEADER SECTION ================-->
+        <?php include("header.php"); ?>
+        <!--=========== END HEADER SECTION ================-->  
+        <style>
+    #details-table td{
+        height: 50px;
+        padding: 0px 30px;
+    }
+
+    .trheding{
+
+        font-size: 13px;
+        color: #FF4800;
+        font-weight: bold;
+        text-transform: uppercase;
+        background:  rgb(243, 243, 243) none repeat scroll 0% 0%;
+
+
+    }
+
+    .subheadng{
+        font-size: 13px;
+        font-weight: bold;
+
+    }
+
+    .cntn{
+
+        border: 1px solid rgba(128, 128, 128, 0.12);
+        border-radius: 4px;
+    }
+
+    .profile-nav > li{
+        border-radius: 0px !important;
+    }
+    .profile-nav > li> a{
+        color: #595959;
+        text-decoration: none;
+        background: rgb(243, 243, 243) none repeat scroll 0% 0%;
+    }
+
+</style>
+        <section id="blogArchive">      
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="blog-breadcrumbs-area">
+                        <div class="container">
+                            <div class="blog-breadcrumbs-left">
+                                <h2>Sign In</h2>
+                            </div>
+                            <div class="blog-breadcrumbs-right">
+                                <ol class="breadcrumb">
+                                    <li>You are here</li>
+                                    <li><a href="#">Home</a></li>                  
+                                    <li class="active">Sign In</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </div>      
+        </section>
+        <div class="col-md-12">
+          <div class="row">
+        <div class="col-md-3"   style="background-color: rgba(210, 210, 210, 0.09);  min-height: 553px" >
+            <div class="col-md-offset-1 col-md-10" ><h3 class="text-center">Arun Thomas</h3></div>
+            <div class="col-md-offset-1 col-md-10" style="padding-bottom: 25px; border-bottom: 1px solid #ddd; ">
+               
+            </div>
+            <div class="col-md-offset-1 col-md-10 text-center" style='padding-top: 10px;'><h3><span style="color:#FF4800;"><?php echo "Arun"; ?></span></h3></div>
+            <div class="col-md-offset-1 col-md-10 text-center" style='padding-top: 0px;'><h4><span style="color:rgb(77, 80, 89);"><?php 
+    echo 'ADMIN';
+
+ ?></span></h4></div>
+
+                <div class="col-md-offset-1 col-md-10 center" ><a href=" ?"><button class='btn btn-success col-md-12'>Edit Profile</button></a></div>
+
+            <div class="clearfix"></div>
+        </div>
+        <div class="col-md-9" style=" border-left: 1px solid #ddd; min-height: 553px">
+                        <div class="col-md-10"><h1>Student Profile</h1></div>
+            <div class="col-md-12" style="margin-top: 10px;">
+                <div class="bhoechie-tab-menu">
+                    <ul class="nav nav-tabs profile-nav">
+                        <li role="presentation" class="active"><a href="#">Profile</a></li>
+                     <li role="presentation"><a href="">Academic Year</a></li> 
+                        <li role="presentation"><a href="#">Classes</a></li>
+
+                    </ul>
+                </div>
+                <div class="col-md-12 bhoechie-tab-content" style="padding: 0px;">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" id="details-table" style="margin-top: 10px; margin-bottom: 10px;">
+                        <tbody>
+                            <tr class="trheding">
+                                <td id="dtr">General</td>
+                                <td id="dtr">&nbsp;</td>
+                                <td id="dtr">&nbsp;</td>
+                                <td id="dtr">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td class="subheadng">Name</td>
+                                <td class="normal">Arun Thomas</td>
+                                <td class="subheadng">&nbsp;  </td>
+                                <td class="normal">&nbsp; </td>   
+                            </tr>
+
+                            <tr>
+                                <td class="subheadng">User Name</td>
+                                <td class="normal">ArunThms</td>
+                                <td class="subheadng">&nbsp;  </td>
+                                <td class="normal">&nbsp; </td>   
+                            </tr>
+                            <tr>
+
+                                <td class="subheadng">User Role</td>
+                                <td class="normal">System Administrator</td>
+                                <td class="subheadng">&nbsp;  </td>
+                                <td class="normal">&nbsp; </td>   
+                            </tr>
+                            <tr>
+                                <td class="subheadng">Email</td>
+                                <td class="normal">A@g.com</td>
+                                <td class="subheadng">&nbsp;  </td>
+                                <td class="normal">&nbsp; </td>   
+                            </tr>
+
+
+
+
+                        </tbody></table>
+                    <br><br>
+                    <div class="clearfix"></div>
+                </div>
+                 
+                <div class="bhoechie-tab-content hide">
+                    <br>
+                        
+
+                            <?php
+                                    
+                                ?>
+
+                                 Buttons for Modifications 
+                               
+
+
+
+            </div>
+                 
+        </div>
+    </div>
+              <script>
+    $(document).ready(function () {
+        $("div.bhoechie-tab-menu>ul>li").click(function (e) {
+            e.preventDefault();
+            $(this).siblings('li.active').removeClass("active");
+            $(this).addClass("active");
+            var index = $(this).index();
+            $("div.bhoechie-tab-content").addClass("hide");
+            $("div.bhoechie-tab-content").eq(index).removeClass("hide");
+        });
+    });
+</script>
+</div>
+
+        </div>
+
+        <?php include 'footer.php'; ?>
+
+        <!-- jQuery Library  -->
+        <script src="js/jquery.js"></script>    
+        <!-- Bootstrap default js -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- slick slider -->
+        <script src="js/slick.min.js"></script>    
+        <script type="text/javascript" src="js/modernizr.custom.79639.js"></script>      
+        <!-- counter -->
+        <script src="js/waypoints.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <!-- Doctors hover effect -->
+        <script src="js/snap.svg-min.js"></script>
+        <script src="js/hovers.js"></script>
+
+        <!-- Custom JS -->
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
