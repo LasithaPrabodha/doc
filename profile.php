27fd45d95345779c6b0fb35e44841132f52774c4
Arun Thomas <arunthms01@gmail.com>
2016-02-28 00:06:11 +0530

    profile update implemented

--- profile.php
+++ profile.php
@@ -1,6 +1,15 @@
 
-<?php include("includes/header.php"); ?>
-<?php include("includes/sql.php"); ?>
+<?php include_once("includes/header.php"); ?>
+<?php
+include_once("includes/sql.php");
+
+//if(!loggedin())
+//{
+?>
+<!--  <script type="text/javascript">
+window.location.href = 'signin.php';
+</script>-->
+//
 <!--=========== END HEADER SECTION ================-->  
 <style>
     #details-table td{
@@ -39,6 +48,10 @@
         text-decoration: none;
         background: rgb(243, 243, 243) none repeat scroll 0% 0%;
     }
+    #profile-img{
+        max-width: 150px;
+        max-height: 150px;
+    }
 
 </style>
 <section id="blogArchive">      
@@ -61,7 +74,54 @@
         </div>        
     </div>      
 </section>
+
 <div class="col-md-12">
+    <div class="row">
+        <?php
+        if (isset($_POST['submit1'])) {
+
+
+
+            $fname = $_POST['first_name'];
+            $lname = $_POST['last_name'];
+            $contact = $_POST['contact_no'];
+            $initials = $_POST['initials'];
+
+            if ((!empty($fname)) && (!empty($lname)) && (!empty($contact)) && (!empty($initials))) {
+
+
+//profile picture upload>>>>>>>>>>>>>
+                $target_dir = "images/";
+                $target_file = $target_dir . basename($_FILES["profile_img"]["name"]);
+                $uploadOk = 1;
+                $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 1) {
+                   
+                    $target_image = str_replace(" ", "_", "$target_file");
+                    if (move_uploaded_file($_FILES["profile_img"]["tmp_name"], $target_image)) {
+
+                        //if file moved to the images folder update the user table
+                        $sql = "UPDATE user SET profile_img='$target_image',first_name='$fname',last_name='$lname', name_with_initials='$initials', contact_number='$contact' WHERE email='a@g.com'";
+                        
+                        if ($result = $conexion->query($sql)) {
+                            echo "<div class='alert alert-success'>Profile settings updated successfully.</div>";
+                        } else {
+                            echo "<div class='alert alert-danger'>Error while updating profile settings.</div>";
+                        }
+                    } else {
+                        //if file not moved to the images folder 
+                        echo "<div class='alert alert-danger'>Sorry, there was an error uploading your profle image.</div>";
+                    }
+                }
+            } else {
+                //if user doesn't enter first name or last name
+                echo "<div class='alert alert-danger'>First name and Last name cannot be empty.</div>";
+            }
+        }
+        ?>
+    </div>
     <div class="row">
         <?php
         $conexion = db_connect();
@@ -87,15 +147,19 @@
         }
         ?>
         <div class="col-md-3"   style="background-color: rgba(210, 210, 210, 0.09);  min-height: 553px" >
-            <div class="col-md-offset-1 col-md-10" ><h3 class="text-center"><?php echo $name_with_initials;?></h3></div>
+            <div class="col-md-offset-1 col-md-10" ><h3 class="text-center"><?php echo $name_with_initials; ?></h3></div>
             <div class="col-md-offset-1 col-md-10" style="padding-bottom: 25px; border-bottom: 1px solid #ddd; align-content:center; ">
-                <image style="width:90%;height: 90%;" src="images/default_prof.jpg"/>
+                <image style="width:80%;height: 80%; margin-left: 20px;" src="<?php if (isset($profile_img) && (!empty($profile_img))) {
+                        echo $profile_img;
+                    } else {
+                        echo "images/default_prof.jpg";
+                    }?>"/>
             </div>
             <div class="col-md-offset-1 col-md-10 text-center" style='padding-top: 10px;'><h3><span style="color:#FF4800;"><?php echo $first_name; ?></span></h3></div>
             <div class="col-md-offset-1 col-md-10 text-center" style='padding-top: 0px;'><h4><span style="color:rgb(77, 80, 89);"><?php
-        if($user_type == 'A'){
+        if ($user_type == 'A') {
             echo 'ADMIN';
-        }elseif ($user_type == 'D') {
+        } elseif ($user_type == 'D') {
             echo 'DOCTOR';
         } elseif ($user_type == 'P') {
             echo 'PATIENT';
@@ -104,21 +168,22 @@
         }
         ?></span></h4></div>
 
-            <div class="col-md-offset-1 col-md-10 center" ><a href=" ?"><button class='btn btn-success col-md-12'>Edit Profile</button></a></div>
+            <div class="col-md-offset-1 col-md-10 center" ><button class='btn btn-success col-md-12' data-toggle="modal" data-target="#editModal">Edit Profile</button></div>
 
             <div class="clearfix"></div>
         </div>
+
         <div class="col-md-9" style=" border-left: 1px solid #ddd; min-height: 553px">
             <div class="col-md-10"><h1><?php
-        if($user_type == 'A'){
-            echo 'Admin Profile';
-        }elseif ($user_type == 'D') {
-            echo 'Doctor Profile';
-        } elseif ($user_type == 'P') {
-            echo 'Patient Profile';
-        } elseif ($user_type == 'G') {
-            echo 'General Pysicient';
-        }
+                        if ($user_type == 'A') {
+                            echo 'Admin Profile';
+                        } elseif ($user_type == 'D') {
+                            echo 'Doctor Profile';
+                        } elseif ($user_type == 'P') {
+                            echo 'Patient Profile';
+                        } elseif ($user_type == 'G') {
+                            echo 'General Pysicient';
+                        }
         ?></h1></div>
             <div class="col-md-12" style="margin-top: 10px;">
                 <div class="bhoechie-tab-menu">
@@ -140,27 +205,35 @@
                             </tr>
                             <tr>
                                 <td class="subheadng">Name</td>
-                                <td class="normal">Arun Thomas</td>
+                                <td class="normal"><?php echo $name_with_initials;?></td>
                                 <td class="subheadng">&nbsp;  </td>
                                 <td class="normal">&nbsp; </td>   
                             </tr>
 
                             <tr>
                                 <td class="subheadng">User Name</td>
-                                <td class="normal">ArunThms</td>
+                                <td class="normal"><?php echo $email; ?></td>
                                 <td class="subheadng">&nbsp;  </td>
                                 <td class="normal">&nbsp; </td>   
                             </tr>
                             <tr>
 
                                 <td class="subheadng">User Role</td>
-                                <td class="normal">System Administrator</td>
+                                <td class="normal"><?php if ($user_type == 'A') {
+                            echo 'Admin';
+                        } elseif ($user_type == 'D') {
+                            echo 'Doctor';
+                        } elseif ($user_type == 'P') {
+                            echo 'Patient';
+                        } elseif ($user_type == 'G') {
+                            echo 'General Pysicient';
+                        }?></td>
                                 <td class="subheadng">&nbsp;  </td>
                                 <td class="normal">&nbsp; </td>   
                             </tr>
                             <tr>
-                                <td class="subheadng">Email</td>
-                                <td class="normal">A@g.com</td>
+                                <td class="subheadng">Contact No</td>
+                                <td class="normal"><?php echo $contact_number; ?></td>
                                 <td class="subheadng">&nbsp;  </td>
                                 <td class="normal">&nbsp; </td>   
                             </tr>
@@ -179,7 +252,7 @@
 
 
 
-                    
+
 
 
 
@@ -191,7 +264,7 @@
 
 
 
-                    
+
 
 
 
@@ -200,22 +273,99 @@
 
             </div>
         </div>
-      
+
     </div>
 
 </div>
+<!-- edit profile -->
+<div class="modal fade bs-modal-lg" id="editModal"  tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="gridSystemModalLabel">Edit Your Profile Details</h4>
+            </div>
+            <div class="modal-body">
 
+                <div class="col-md-9 col-md-offset-1">
+                    <form name="prof-settings" id="prof-settings" action="" method="post" enctype="multipart/form-data">
+                        <div class="fom-group img-submit">
+                            <label for="profile-img">Profile image</label>
+                            <br />
+
+                            <img src="<?php
+                    if (isset($profile_img) && (!empty($profile_img))) {
+                        echo $profile_img;
+                    } else {
+                        echo "images/default_prof.jpg";
+                    }
+        ?>" id="profile-img" class="img-thumbnail profile-img" name="profile-img">
+                            <span class="btn btn-default btn-file">
+                                Upload new picture<input type="file" name="profile_img" id="profile_img">
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" name="first_name" id="first_name" class="form-control" value="<?php echo $first_name; ?>">
+                            <p class="help-block"></p>
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" name="last_name" id="last_name" class="form-control" value="<?php echo $last_name; ?>">
+                            <p class="help-block"></p>
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name">Name With Initials</label>
+                            <input type="text" name="initials" id="initials" class="form-control" value="<?php echo $name_with_initials; ?>">
+                            <p class="help-block"></p>
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name">Contact Number</label>
+                            <input type="text" name="contact_no" id="contact_no" class="form-control" value="<?php echo $contact_number; ?>">
+                            <p class="help-block"></p>
+                        </div>
+                        <input type="submit" class="btn btn-success" value="Submit" name="submit1">
+
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <!--        <button type="button" class="btn btn-primary">Save changes</button>-->
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <!-- end of edit profile -->
 <?php include 'includes/footer.php'; ?>
 
-  <script>
-            $(document).ready(function () {
-                $("div.bhoechie-tab-menu>ul>li").click(function (e) {
-                    e.preventDefault();
-                    $(this).siblings('li.active').removeClass("active");
-                    $(this).addClass("active");
-                    var index = $(this).index();
-                    $("div.bhoechie-tab-content").addClass("hide");
-                    $("div.bhoechie-tab-content").eq(index).removeClass("hide");
-                });
+    <script>
+        $(document).ready(function () {
+            $("div.bhoechie-tab-menu>ul>li").click(function (e) {
+                e.preventDefault();
+                $(this).siblings('li.active').removeClass("active");
+                $(this).addClass("active");
+                var index = $(this).index();
+                $("div.bhoechie-tab-content").addClass("hide");
+                $("div.bhoechie-tab-content").eq(index).removeClass("hide");
             });
-        </script>
+        });
+
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#profile-img').attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#profile_img").change(function () {
+            readURL(this);
+        });
+
+
+    </script>
