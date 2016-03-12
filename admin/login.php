35edd9bdfc11cbfcc84debe0ca9f83c9100f7b0f
lasithaprabodha <lasiprabo@gmail.com>
2016-03-13 00:29:08 +0530

    minor changes

--- admin/login.php
+++ admin/login.php
@@ -1,56 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!--
-        ===
-        This comment should NOT be removed.
+<?php 
 
-        Charisma v2.0.0
+require_once("../includes/functions.php");
+include_once 'header.php';
 
-        Copyright 2012-2014 Muhammad Usman
-        Licensed under the Apache License v2.0
-        http://www.apache.org/licenses/LICENSE-2.0
+if(isset($_POST['username']) && isset($_POST['password']))
+{
+    $LUsername = sql_escape($_POST['username']);
+    $LPassword = sql_escape($_POST['password']);
 
-        http://usman.it
-        http://twitter.com/halalit_usman
-        ===
-    -->
-    <meta charset="utf-8">
-    <title>Free HTML5 Bootstrap Admin Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
-    <meta name="author" content="Muhammad Usman">
+    if(!empty($LUsername) && !empty($LPassword))
+    {
+        $hashPassword =md5($LPassword);
+        $conexion = db_connect();
+        $sql = "SELECT * FROM user WHERE email = '{$LUsername}' ";
+        $result = $conexion->query($sql);
+        $resultSet[] = null;
+        if ($result->num_rows > 0) {
 
-    <!-- The styles -->
-    <link id="bs-css" href="css/bootstrap-cerulean.min.css" rel="stylesheet">
+            $resultSet = $result->fetch_assoc();
+        }
 
-    <link href="css/charisma-app.css" rel="stylesheet">
-    <link href='bower_components/fullcalendar/dist/fullcalendar.css' rel='stylesheet'>
-    <link href='bower_components/fullcalendar/dist/fullcalendar.print.css' rel='stylesheet' media='print'>
-    <link href='bower_components/chosen/chosen.min.css' rel='stylesheet'>
-    <link href='bower_components/colorbox/example3/colorbox.css' rel='stylesheet'>
-    <link href='bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
-    <link href='bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
-    <link href='css/jquery.noty.css' rel='stylesheet'>
-    <link href='css/noty_theme_default.css' rel='stylesheet'>
-    <link href='css/elfinder.min.css' rel='stylesheet'>
-    <link href='css/elfinder.theme.css' rel='stylesheet'>
-    <link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
-    <link href='css/uploadify.css' rel='stylesheet'>
-    <link href='css/animate.min.css' rel='stylesheet'>
+        if(!empty($resultSet))
+        {
+            $filteredID = sql_escape($resultSet['email']);
+            $filteredPW = sql_escape($resultSet['password']);
+            if((($filteredID == $LUsername)) && ($filteredPW == $hashPassword))
+            {
 
-    <!-- jQuery -->
-    <script src="bower_components/jquery/jquery.min.js"></script>
+                $_SESSION['email']=$filteredID;
+                $_SESSION['user_id']=$resultSet['user_id'];
+                $_SESSION['first_name'] = $resultSet['first_name'];
+                $_SESSION['last_name'] = $resultSet['last_name'];
+                $_SESSION['name_with_initials'] = $resultSet['name_with_initials'];
+                $_SESSION['user_type'] = $resultSet['user_type'];
+                $_SESSION['is_active'] = $resultSet['is_active'];
 
-    <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- The fav icon -->
-    <link rel="shortcut icon" href="img/favicon.ico">
-
-</head>
+                header("Location:/doc/admin/index.php");
+            }else{
+                echo "<div>";
+                echo " <h2 style='text-align: center;top: 355px;position: absolute;left: 0;margin: auto;width: 100%;'><font color=red>Wrong Password</font></h2>";
+                echo "</div>";
+            }
+        }else{
+            echo "<div>";
+            echo " <h2 style='text-align: center;top: 362px;position: absolute;left: 0;margin: auto;width: 100%;'><font color=red>User Not Found</font></h2>";
+            echo "</div>";
+        }
+    }
+}
+?>
 
 <body>
 <div class="ch-container">
@@ -68,17 +68,17 @@
             <div class="alert alert-info">
                 Please login with your Username and Password.
             </div>
-            <form class="form-horizontal" action="index.html" method="post">
+            <form class="form-horizontal" action="" method="post">
                 <fieldset>
                     <div class="input-group input-group-lg">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user red"></i></span>
-                        <input type="text" class="form-control" placeholder="Username">
+                        <input type="text" class="form-control" placeholder="Username" name="username" id="username">
                     </div>
                     <div class="clearfix"></div><br>
 
                     <div class="input-group input-group-lg">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock red"></i></span>
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input type="password" class="form-control" placeholder="Password" name="password" id="password">
                     </div>
                     <div class="clearfix"></div>
 
--- admin/topBar.php
+++ admin/topBar.php
@@ -20,7 +20,7 @@
                 <ul class="dropdown-menu">
                     <li><a href="#">Profile</a></li>
                     <li class="divider"></li>
-                    <li><a href="login.php">Logout</a></li>
+                    <li><a href="../signout.php">Logout</a></li>
                 </ul>
             </div>
             <!-- user dropdown ends -->
